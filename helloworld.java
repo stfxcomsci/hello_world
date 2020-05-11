0c285f9b215fd2d0443a1010cc962cc8fa80edee
Greg.L.Reid <greg.l.reid@gmail.com>
2020-05-11 15:18:45 -0400

    Added programs

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+/**
+ * Test github with Hello World
+ * @author G Reid
+ * @date 2020/05/11
+ */
+public class helloworld {
+
+   public static void main(String args[]) {
+      System.out.println("Hello World");   
+   }   
+}
---
+++ helloworld.py
@@ -0,0 +1,7 @@
+
+## Test Git - Hello World
+# @author G Reid
+# @date 2020/05/11
+
+name = input("What is your name?: ")
+print("Hello World from", name)
